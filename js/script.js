451fb3bb8382fa9466837141828368fe1f3dc822
Rampranav <vrampranav9@gmail.com>
2020-12-02 22:32:49 +0530

    Tested Using Mock Server

--- js/script.js
+++ js/script.js
@@ -4,6 +4,15 @@ var btnTrans = document.querySelector("#btnTranslate");
 var outputText = document.querySelector("#output-text");
 var clearBtn = document.querySelector("#clrBtn");
 
+var mockServerUrl = 'https://mockserver.vrampranav.repl.co/translate/rp.json';
+//function to append userInput to url
+function urlGenerator(text) {
+    return mockServerUrl + '?text=' + text;
+};
+//Function Error
+function catchError(e) {
+    console.log("Error Occured" + e);
+}
 // TranslateBtn Click Event
 btnTrans.addEventListener('click', () => {
     var userText = userInput.value;
@@ -12,15 +21,28 @@ btnTrans.addEventListener('click', () => {
         alert("Oops! Miniator didn't receive any text...");
         return;
     }
-    outputText.innerText = userText;
-    outputText.style.color = 'black'
+
+    //Fetching the translation from the mock server
+    fetch(urlGenerator(userText.trim()))
+        .then(response => response.json())
+        .then(jsonResponse => {
+            outputText.innerText = jsonResponse.contents.translated;
+        })
+        .catch(e => catchError(e))
+
+    //Changing the output color to black, timeout is used as the response from server slightly gets delayed!
+    setTimeout(() => {
+        outputText.style.color = 'black'
+    }, 400);
+    // Making the clear button visible after geting translation!
     clearBtn.style.visibility = 'visible';
 });
 
 // Clear Button Click Event
 clearBtn.addEventListener('click', () => {
     userInput.value = '';
-    outputText.innerText = "";
+    outputText.innerText = "Minion Text will come here!";
+    outputText.style.color = 'gray';
     clearBtn.style.visibility = 'hidden';
 });
 
